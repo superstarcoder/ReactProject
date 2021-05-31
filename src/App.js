5ab249a95b6fec309720d11db30a94d1dfaeba8d
superstarcoder <dhanishnatarajan@gmail.com>
2021-05-31 12:53:56 -0700

    added eslint configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, {useState} from "react";
+
+
+// const App = () => {
+//   // const [{count, count2}, setCount] = useState({count:10, count2: 20});
+//   const [count, setCount] = useState(20); 
+//   const [count2, setCount2] = useState(20); 
+
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           // setCount(currentState => ({
+//           //   count2: currentState.count2,
+//           //   count: currentState.count + 1
+//           //   }) )
+
+//           setCount(c => c+1);
+//           setCount2(c => c+2);
+//           }
+//         }>
+//         +
+//       </button>
+
+//       <div>count1: {count}</div>
+//       <div>count2: {count2}</div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
 }
-
 export default App;
